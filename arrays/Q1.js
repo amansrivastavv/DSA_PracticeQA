9cc4c267364d843efb08bef50ca4db7a083846b3
Aman <amansrivastavvv@gmai.com>
2025-07-25 00:29:46 +0530

    solve Find the Maximum Number in an Array

---
+++ arrays/Q1.js
@@ -0,0 +1,13 @@
+// Find the Maximum Number in an Array
+
+function findMax(arr) {
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
+console.log(findMax([4, 2, 8, 9, 1, 3, 6]));  
